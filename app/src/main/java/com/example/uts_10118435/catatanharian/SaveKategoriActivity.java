13ec1a63fdcf45bee2f8169662d1d371e7c6fc27
saefulapriana99 <saeful.apriana@gmail.com>
2021-06-07 06:32:55 +0700

    tugas AKB 10118435

---
+++ app/src/main/java/com/example/uts_10118435/catatanharian/SaveKategoriActivity.java
@@ -0,0 +1,53 @@
+package com.example.uts_10118435.catatanharian;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.uts_10118435.R;
+import com.example.uts_10118435.base.DBHelper;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+//07 juni
+//saefulapriana 10118435
+public class SaveKategoriActivity extends AppCompatActivity {
+
+    private EditText titleInput;
+    private EditText contentInput;
+    private Button btn_simpan, btn_kembali;
+    DBHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_kategori);
+
+        dbHelper = new DBHelper(this);
+        titleInput = findViewById(R.id.input_title);
+        contentInput = findViewById(R.id.input_content);
+        btn_simpan = findViewById(R.id.button_submit);
+        btn_kembali = findViewById(R.id.button_back);
+
+        btn_simpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                db.execSQL("insert into kategori(kategori) values('" + titleInput.getText().toString() + "')");
+                Toast.makeText(getApplicationContext(), "Kategori Berhasil Dibuat", Toast.LENGTH_LONG).show();
+                KategoriActivity.ma.RefreshList();
+                finish();
+            }
+        });
+
+        btn_kembali.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/uts_10118435/dashboard/DashboardActivity.java
@@ -0,0 +1,71 @@
+package com.example.uts_10118435.dashboard;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.uts_10118435.R;
+import com.example.uts_10118435.catatanharian.KategoriActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+//07 juni
+//saefulapriana 10118435
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+        //Initialize And Assign Variable
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        //Set Home Selected
+        bottomNavigationView.setSelectedItemId(R.id.ProfileId);
+
+        //Perform ItemSelectedListener
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.ProfileId:
+                        startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.CatatanHarian:
+                        startActivity(new Intent(getApplicationContext(), KategoriActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+                    case R.id.About:
+                        final Dialog dialog = new Dialog(DashboardActivity.this);
+
+                        //Memasang Title / Judul pada Custom Dialog
+                        dialog.setTitle("About");
+
+                        //Memasang Desain Layout untuk Custom Dialog
+                        dialog.setContentView(R.layout.custom_design_dialog);
+
+                        //Memasang Listener / Aksi saat tombol OK di Klik
+                        Button DialogButton = dialog.findViewById(R.id.ok);
+                        DialogButton.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                dialog.dismiss();
+                            }
+                        });
+
+                        dialog.show();
+                        return true;
+                }
+
+                return false;
+            }
+        });
+    }
+}
